744ea4c2b1877a5bf46217d8dbf6959b5ffc341e
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2014-07-23 00:31:04 +0900

    engine: Add setup function class for KiwiCode library

--- Source/PZMain.m
+++ Source/PZMain.m
@@ -8,10 +8,12 @@
 #import <UIKit/UIKit.h>
 
 #import "PZAppDelegate.h"
+#import <KiwiCode/KiwiCode.h>
 
 int main(int argc, char * argv[])
 {
 	@autoreleasepool {
-	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([PZAppDelegate class]));
+		KCSetupFramework() ;
+		return UIApplicationMain(argc, argv, nil, NSStringFromClass([PZAppDelegate class]));
 	}
 }
